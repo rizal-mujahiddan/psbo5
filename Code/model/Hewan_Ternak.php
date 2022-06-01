17a99535e1469337887681da178d257dee6e49e8
ftmhalf <ftmhalfijannah@apps.ipb.ac.id>
2022-06-01 15:01:21 +0700

    buat class hewan ternak dan sapi

--- Code/model/Hewan_Ternak.php
+++ Code/model/Hewan_Ternak.php
@@ -1,13 +1,41 @@
 <?php
     abstract class Hewan_Ternak{
-        public $nama;
-        protected $berat;
-        protected $tinggi;
-        public function __construct($nama, $berat, $tinggi) {
-            $this->nama = $nama;
-            $this->$berat = $berat;
-            $this->$tinggi = $tinggi;
+        public $id;
+        protected $jenkel;
+        protected $umur;
+        protected $kesehatan;
+        protected $bobot;
+        protected $pakan;
+        protected $masuk;
+        protected $keluar;
+        protected $statusjual;
+        public function __construct($id, $jenkel, $umur, $kesehatan, $bobot, $pakan, $masuk, $keluar, $statusjual) {
+            $this->$id = $id;
+            $this->$jenkel = $jenkel;
+            $this->$umur = $umur;
+            $this->$kesehatan = $kesehatan;
+            $this->$bobot = $bobot;
+            $this->$pakan = $pakan;
+            $this->$masuk = $masuk;
+            $this->$keluar = $keluar;
+            $this->$statusjual = $statusjual;
         }
-        abstract public function bunyi(): string;
+        abstract public function status(): string;
+        abstract public function produce(): string;
+        abstract public function health(): bool;
     }
+
+    // class Sapi extends Hewan_Ternak{
+    //     protected $bobotmeatsapi;
+    // }
+
+    // class Unggas extends Hewan_Ternak{
+    //     protected $bobotmeatunggas;
+    //     protected $jumlahegg;
+    // }
+
+    // class Kambing extends Hewan_Ternak{
+    //     protected $bobotmeatkambing;
+    //     protected $bobothair;
+    // }
 ?>
---
+++ Code/model/Sapi.php
@@ -0,0 +1,11 @@
+<?php
+    class Sapi extends Hewan_Ternak{
+        protected $bobotmeatsapi;
+
+    }
+
+    class Sapi_Perah extends Sapi{
+
+    }
+
+?>
