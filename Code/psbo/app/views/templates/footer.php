343ff8f0d51dda97dfd9d9bb98bfa311c1061617
rizal-mujahiddan <rizal.mujahiddan@gmail.com>
2022-06-18 09:19:08 +0700

    r

--- Code/psbo/app/views/templates/footer.php
+++
@@ -1,6 +0,0 @@
-   
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
-  </body>
-</html>
--- index.php
+++ index.php
@@ -1,4 +1,13 @@
-<?php 
+<!DOCTYPE HTML>
+<html>
+<head></head>
+<body>
+<!-- <?php 
     require_once 'app/init.php';
     header('Location: ' . BASEURL);
-?>
+?> -->
+<div>
+    Rizal Mujahiddan Adalah Manusia keren
+</div>
+<body>
+</html>
