64b6331531d5e9f35cf98e5c116a8b414a8d9de4
rizal-mujahiddan <rizal.mujahiddan@gmail.com>
2022-06-18 09:28:28 +0700

    htaccess

--- index.php
+++ index.php
@@ -1,9 +1,4 @@
-<!DOCTYPE HTML>
-<html>
-<head></head>
-<body>
-<div>
-    Rizal Mujahiddan Adalah Manusia keren
-</div>
-<body>
-</html>
+<?php 
+    require_once 'app/init.php';
+    header('Location: ' . BASEURL);
+?>
